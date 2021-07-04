3ed40dcc1a261ef8619bf1d2d81cde9b551108f9
vandakiara <vandabarata91@gmail.com>
2021-07-04 23:14:42 +0100

    Add test for notification about age != 30

---
+++ cypress/integration/main_spec.js
@@ -0,0 +1,18 @@
+describe("vax age", () => {
+
+    it("should look for something younger than 30", () => {
+        cy.visit("/")
+        
+        cy.get("h3")
+        .invoke("text")
+        .should((title) => {
+            if (title.includes("30")) {
+                expect("false").to.equal("true")
+            } 
+            else (
+                expect("true").to.equal("true") 
+            )
+        })
+    });
+
+});
